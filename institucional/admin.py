d5bf07f659e9c9ea254cb8970e65f00ea6592705
Rary Gonçalves <40248598+goncalvesrary@users.noreply.github.com>
2019-10-10 18:52:33 -0300

    #02 - Atualização total - Inserção de novos apps
    Adição de apps Institucional, Acomodações e exclusão de app Cards

---
+++ institucional/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Quemsomos
+
+# Register your models here.
+
+admin.site.register(Quemsomos)
---
+++ institucional/models.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+
+class Quemsomos(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.title
