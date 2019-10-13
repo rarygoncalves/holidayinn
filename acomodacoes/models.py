5c937653a60a106d95ae3f9dae56f43abc5498e5
Rary Gonçalves <40248598+goncalvesrary@users.noreply.github.com>
2019-10-13 00:28:55 -0300

    #05 - Atualização dos Models de Acomodações
    Foi inserido os campos para informar a capacidade do quarto, bem como informar se pode ou não fumar no quarto. Também foi removido a opção de preço das promoções, tendo em vista que não haverá promoções relativas à preços no site.

--- acomodacoes/models.py
+++ acomodacoes/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Promocoes(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=200)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    # price = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
         return self.title
@@ -19,6 +19,8 @@ class Acomodacoes(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=200)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    smoke = models.BooleanField(default=True)
+    peoples = models.DecimalField(max_digits=2, decimal_places=0)
 
     def __str__(self):
         return self.title
